a2f4743884a900e4d898693b6bd951d62cbe9bd9
jramma <jrmasip097@gmail.com>
2025-02-14 16:17:25 +0100

    ↗️ tailwind 4.0

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+
 import tailwindcss from "@tailwindcss/vite";
-// https://astro.build/config
+
 export default defineConfig({
   site: "https://example.com",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [icon()],
+  vite: { plugins: [tailwindcss()] },
 });
